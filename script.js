abce17e7090342aa0139662db7e403b79beb0e9b
emillyrosamendes-web <emilly.rosa.mendes@escola.pr.gov.br>
2026-05-27 09:37:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarMensagem(){
+
+  const mensagem = document.getElementById("mensagem");
+
+  mensagem.innerHTML =
+    "A educação é essencial para combater o analfabetismo e reduzir as desigualdades sociais no Brasil.";
+
+}
