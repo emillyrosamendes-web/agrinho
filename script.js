a0fed429d72781e031b40c77003d1b4221799fb0
emillyrosamendes-web <emilly.rosa.mendes@escola.pr.gov.br>
2026-06-01 09:20:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-function mostrarMensagem(){
+function calcular() {
 
-  const mensagem = document.getElementById("mensagem");
+    let residuos = parseFloat(
+        document.getElementById("residuos").value
+    );
 
-  mensagem.innerHTML =
-    "A educação é essencial para combater o analfabetismo e reduzir as desigualdades sociais no Brasil.";
+    let dias = parseInt(
+        document.getElementById("dias").value
+    );
 
+    const resultado =
+        document.getElementById("resultado");
+
+    if (
+        isNaN(residuos) ||
+        isNaN(dias) ||
+        residuos <= 0 ||
+        dias <= 0
+    ) {
+        resultado.innerHTML =
+            "⚠️ Insira valores válidos.";
+        return;
+    }
+
+    let total = residuos * dias;
+
+    // Aproximadamente 40% vira composto
+    let adubo = total * 0.4;
+
+    resultado.innerHTML = `
+        ✅ Você evitou que
+        <strong>${total.toFixed(1)} kg</strong>
+        de resíduos fossem enviados aos aterros.
+        <br><br>
+
+        🌿 Estimativa de adubo produzido:
+        <strong>${adubo.toFixed(1)} kg</strong>.
+    `;
 }
